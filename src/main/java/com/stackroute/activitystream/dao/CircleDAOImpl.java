216377d96d59980ea7f55b3dcf01a8ac5c9eeb42
abbas886 <abbas886@yahoo.com>
2017-08-10 10:43:56 +0530

    Added Comments.
    Need to discuss regarding what are the methods should be available in this DAO.

--- src/main/java/com/stackroute/activitystream/dao/CircleDAOImpl.java
+++ src/main/java/com/stackroute/activitystream/dao/CircleDAOImpl.java
@@ -24,13 +24,17 @@ public class CircleDAOImpl implements CircleDAO {
 		} catch (HibernateException e) {
 			e.printStackTrace();
 		}
+		//in case of exception, you can return false.  So return false should be in catch block
 		return false;
 	}
 
 	@Override
 	public boolean addUserToCircle(String emailId, String circleName) {
 		try {
+			//Try to avoid creating object by using 'new' operator
 			Circle circle = new Circle();
+			//what is this owner emailID.  Method name says addUserToCircle.
+			//You already defined addCircle in this DAO.What this method doing?
 			circle.setOwnerEmailId(emailId);
 			circle.setCircleName(circleName);
 			sessionFactory.getCurrentSession().save(circle);
@@ -39,17 +43,22 @@ public class CircleDAOImpl implements CircleDAO {
 
 			e.printStackTrace();
 		}
+		//this statement should be in catch statement.
 		return false;
 	}
 
 	@Override
 	public List<Circle> getAllCircles() {
+		//Better to use Criteria if where condition present.
 		String hql="from Circle where status=true";
 		return sessionFactory.getCurrentSession().createQuery(hql).list();
 	}
 
+	//method name should change.
 	@Override
 	public List<Circle> getCircleByUser(String userEmail) {
+		//Better to use Criteria if where condition present.
+		//Do you want to retrieve what are the circles created by a particular person?
 		String sql="from Circle where ownerEmailId='"+userEmail+"'";
 		return sessionFactory.getCurrentSession().createQuery(sql).list();
 	}
